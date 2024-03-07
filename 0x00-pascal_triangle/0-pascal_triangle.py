baa0ced998b176e94c2ee8685e70399b814bc507
sangaryousmane <sangaryousmane@gmail.com>
2024-03-07 16:21:09 +0000

    First make the algorithm works with a double for loop

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """
+    Generates the Pascal's triangle up to the specified number of rows.
+
+    :param
+      n: The number of rows for which to generate the Pascal's triangle.
+    :return:
+      A 2D list representing the Pascal's triangle with 'n' rows.
+    """
+    triangle = [[1]]
+    if n <= 0:
+        return []
+
+    for i in range(n - 1):
+        temp = [0] + triangle[-1] + [0]
+        row = []
+        for j in range(len(triangle[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        triangle.append(row)
+    return triangle
