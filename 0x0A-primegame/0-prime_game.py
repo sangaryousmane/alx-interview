6fbd8fd71f1182b66ce9de24c8b87efa0244508e
sangaryousmane <sangaryousmane@gmail.com>
2024-06-25 18:16:39 -0700

    Update code to handle other constraints

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,59 +8,27 @@ def isWinner(x, nums):
     """ Main Function - determines the winner
     of each game
     """
+    if not nums or x < 1:
+        return
+    max_num = max(nums)
 
-    def is_prime(num):
-        """ Check for prime numbers
-        """
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
-
-    def get_primes_up_to(n):
-        """ Get prime numbers upto certain number - n
-        """
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        return primes
-
-    def play_game(n):
-        """ Play the game
-        """
-        primes = get_primes_up_to(n)
-        maria_turn = True
-        while primes:
-            if maria_turn:
-                if not primes:
-                    return "Ben"
-                chosen_prime = primes[0]
-                primes = [p for p in primes if p % chosen_prime != 0]
-            else:
-                if not primes:
-                    return "Maria"
-                chosen_prime = primes[0]
-                primes = [p for p in primes if p % chosen_prime != 0]
-            maria_turn = not maria_turn
-        return "Ben" if maria_turn else "Maria"
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        winner = play_game(n)
-        if winner == "Maria":
-            maria_wins += 1
-        else:
-            ben_wins += 1
-    if maria_wins > ben_wins:
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
         return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
+    return "Ben"
